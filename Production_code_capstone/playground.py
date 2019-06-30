2567b66c49eea2166058efc689d453c94df8b073
David Stoud <david@Davids-iMac.attlocal.net>
2019-06-30 18:38:19 -0400

    add libraries

---
+++ Production_code_capstone/playground.py
@@ -0,0 +1,10 @@
+# Make sure that you have all these libaries available to run the code successfully
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
