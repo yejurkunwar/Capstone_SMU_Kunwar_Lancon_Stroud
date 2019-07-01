3b7094a9d9e46f5afb31da0415a942d87e32b88a
David Stoud <david@Davids-iMac.attlocal.net>
2019-06-30 20:20:04 -0400

    add stock chart

--- Production_code_capstone/playground.py
+++ Production_code_capstone/playground.py
@@ -57,7 +57,14 @@ else:
     df = pd.read_csv(os.path.join('Production_code_capstone/Data/Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
     print('Loaded data from the Kaggle repository')
 
-
-
+# Sort DataFrame by data
+df = df.sort_values('Date')
 
 df.head()
+
+plt.figure(figsize=(18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500], rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price', fontsize=18)
+plt.show()
